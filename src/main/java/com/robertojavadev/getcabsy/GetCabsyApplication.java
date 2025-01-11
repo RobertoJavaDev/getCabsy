c3c44f541cfbb19842d39105ae6a18d50848803c
RobertoJavaDev <robertojavadev@gmail.com>
2025-01-11 12:04:12 +0100

    Initial commit

---
+++ src/main/java/com/robertojavadev/getcabsy/GetCabsyApplication.java
@@ -0,0 +1,13 @@
+package com.robertojavadev.getcabsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetCabsyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetCabsyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robertojavadev/getcabsy/GetCabsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertojavadev.getcabsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetCabsyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
