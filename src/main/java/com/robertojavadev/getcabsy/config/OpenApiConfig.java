9bbda3f8b0296b0e3032f2adcf4413ec8d4e8406
RobertoJavaDev <robertojavadev@gmail.com>
2025-02-12 09:18:55 +0100

    Configurated Swagger

--- src/main/java/com/robertojavadev/getcabsy/config/OpenApiConfig.java
+++ src/main/java/com/robertojavadev/getcabsy/config/OpenApiConfig.java
@@ -1,4 +1,41 @@
 package com.robertojavadev.getcabsy.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.core.util.Json;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+@Configuration
 class OpenApiConfig {
+
+    @Bean
+    public OpenAPI getOpenAPI() {
+
+        try {
+            Path path = Paths.get("src/main/resources/static/swagger.yaml");
+            InputStream inputStream = Files.newInputStream(path);
+            Yaml yaml = new Yaml();
+            Map<String, Object> yamlMap = yaml.load(inputStream);
+
+            OpenAPI openAPI = convertToOpenAPI(yamlMap);
+
+            return openAPI;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new OpenAPI();
+        }
+    }
+
+    private OpenAPI convertToOpenAPI(Map<String, Object> yamlMap) {
+        ObjectMapper mapper = Json.mapper();
+        return mapper.convertValue(yamlMap, OpenAPI.class);
+    }
 }
